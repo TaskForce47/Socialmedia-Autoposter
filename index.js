a05b4fa3d62f3dca9f32214964215dec283ecb28
Pierre Humberdroz <p.humberdroz@gmail.com>
2017-01-09 07:47:42 +0100

    initial version working

--- index.js
+++ index.js
@@ -1,19 +1,18 @@
 require('dotenv').config();
 var SteamCommunity = require('steamcommunity');
 var community = new SteamCommunity();
-var Globalize = require( "globalize" );
-Globalize.load( require( "cldr-data" ).entireSupplemental() );
-Globalize.load( require( "cldr-data" ).entireMainFor( "de", "en" ) );
+var storage = require('node-persist');
 
-console.log ( process.env.STEAM_ACCOUNTNAME)
-community.login ({"accountName": process.env.STEAM_ACCOUNTNAME,
-    "password": process.env.STEAM_PASSWORD}, function (err){ if (err) {console.log (err)}})
+storage.initSync();
+
+
+
 
 var FeedParser = require('feedparser');
 var request = require('request'); // for fetching the feed
 
 var req = request('https://forum.armasim.de/forum/calendar/index.php/CalendarFeed/')
-var feedparser = new FeedParser();
+var feedparser = new FeedParser({addmeta: false});
 
 req.on('error', function (error) {
   // handle any request errors
@@ -34,6 +33,21 @@ feedparser.on('error', function (error) {
   // always handle errors
 });
 
+community.login (
+	{
+		"accountName": process.env.STEAM_ACCOUNTNAME,
+    	"password": process.env.STEAM_PASSWORD
+	}, function (err){ 
+		if (err) {
+			console.log (err)
+		}
+		community.loggedIn(function (err,loggedIn) {
+			if (err) {
+				console.log(err)
+			}
+			console.log(loggedIn)
+		})
+
 feedparser.on('readable', function () {
   // This is where the action is!
   var stream = this; // `this` is `feedparser`, which is a stream
@@ -41,8 +55,60 @@ feedparser.on('readable', function () {
   var item;
 
   while (item = stream.read()) {
-  	date=item.title.match(/\((.*)\)/)[1].split('-')[0]
-  	date=Globalize.parseDate( date, "D", "de" );
-    console.log(date);
+  	date=item.title.match(/\((.*)\)/)[1].split('-')[0].split(', ')
+    day= date[1].split(' ')[0]
+    month= date[1].split(' ')[1]
+    year= date[1].split(' ')[2]
+	switch (month) {
+	  case "Januar":
+    	month= "January";
+    	break;
+	  case "Februar":
+	    month= "February";
+	    break;
+	  case "März":
+		month= "March";
+		break;
+	  case "Mai":
+	    month= "May";
+	    break;
+	  case "Juni":
+	    month= "June";
+	    break;
+	  case "Juli":
+	    month= "July";
+	    break;
+	  case "Oktober":
+	    month= "October";
+	    break;
+	  case "Dezember":
+	    month= "December";
+	    break;
+	}
+	timestamp=new Date(Date.parse(day + ' ' + month + ' ' + year + ' ' + date[2]))
+	if (!(storage.getItemSync(item.guid))){
+		community.scheduleGroupEvent(
+			"103582791456935650",
+			item.title,
+			"107410",
+			item.description,
+			timestamp,
+			{ip:'46.4.112.40:2322', password:''},
+			function(err) {
+				if (err) {
+					console.log(err)
+				} else {
+					storage.setItemSync(item.guid,true);
+				}
+			}
+		)
+		
+		console.log("posted an event")
+	} else {
+		console.log("already posted this event")
+	}
+
   }
 });
+	}
+)
