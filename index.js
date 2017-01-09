dec571d42cb7a9baf925142ce8e57f776a13c940
Pierre Humberdroz <p.humberdroz@gmail.com>
2017-01-09 12:47:49 +0100

    exit when finished

--- index.js
+++ index.js
@@ -32,7 +32,6 @@ req.on('response', function (res) {
 feedparser.on('error', function (error) {
   // always handle errors
 });
-
 community.login (
 	{
 		"accountName": process.env.STEAM_ACCOUNTNAME,
@@ -48,67 +47,68 @@ community.login (
 			console.log(loggedIn)
 		})
 
-feedparser.on('readable', function () {
-  // This is where the action is!
-  var stream = this; // `this` is `feedparser`, which is a stream
-  var meta = this.meta; // **NOTE** the "meta" is always available in the context of the feedparser instance
-  var item;
+		feedparser.on('readable', function () {
+		  // This is where the action is!
+		  var stream = this; // `this` is `feedparser`, which is a stream
+		  var meta = this.meta; // **NOTE** the "meta" is always available in the context of the feedparser instance
+		  var item;
 
-  while (item = stream.read()) {
-  	date=item.title.match(/\((.*)\)/)[1].split('-')[0].split(', ')
-    day= date[1].split(' ')[0]
-    month= date[1].split(' ')[1]
-    year= date[1].split(' ')[2]
-	switch (month) {
-	  case "Januar":
-    	month= "January";
-    	break;
-	  case "Februar":
-	    month= "February";
-	    break;
-	  case "März":
-		month= "March";
-		break;
-	  case "Mai":
-	    month= "May";
-	    break;
-	  case "Juni":
-	    month= "June";
-	    break;
-	  case "Juli":
-	    month= "July";
-	    break;
-	  case "Oktober":
-	    month= "October";
-	    break;
-	  case "Dezember":
-	    month= "December";
-	    break;
-	}
-	timestamp=new Date(Date.parse(day + ' ' + month + ' ' + year + ' ' + date[2]))
-	if (!(storage.getItemSync(item.guid))){
-		community.scheduleGroupEvent(
-			process.env.STEAM_GROUPID,
-			item.title,
-			"107410",
-			item.description,
-			timestamp,
-			{ip:'46.4.112.40:2322', password:''},
-			function(err) {
-				if (err) {
-					console.log(err)
-				} else {
-					storage.setItemSync(item.guid,true);
-				}
+		  while (item = stream.read()) {
+		  	date=item.title.match(/\((.*)\)/)[1].split('-')[0].split(', ')
+		    day= date[1].split(' ')[0]
+		    month= date[1].split(' ')[1]
+		    year= date[1].split(' ')[2]
+			switch (month) {
+			  case "Januar":
+		    	month= "January";
+		    	break;
+			  case "Februar":
+			    month= "February";
+			    break;
+			  case "März":
+				month= "March";
+				break;
+			  case "Mai":
+			    month= "May";
+			    break;
+			  case "Juni":
+			    month= "June";
+			    break;
+			  case "Juli":
+			    month= "July";
+			    break;
+			  case "Oktober":
+			    month= "October";
+			    break;
+			  case "Dezember":
+			    month= "December";
+			    break;
 			}
-		)
-		
-		console.log("posted an event")
-	} else {
-		console.log("already posted this event")
-	}
+			timestamp=new Date(Date.parse(day + ' ' + month + ' ' + year + ' ' + date[2]))
+			if (!(storage.getItemSync(item.guid))){
+				community.scheduleGroupEvent(
+					process.env.STEAM_GROUPID,
+					item.title,
+					"107410",
+					item.description,
+					timestamp,
+					{ip:'46.4.112.40:2322', password:''},
+					function(err) {
+						if (err) {
+							console.log(err)
+						} else {
+							storage.setItemSync(item.guid,true);
+						}
+					}
+				)
+				
+				console.log("posted an event")
+			} else {
+				console.log("already posted this event")
+			}
 
-  }
-});
+		  }
+		  process.exit()
+		});
 	}
 )
